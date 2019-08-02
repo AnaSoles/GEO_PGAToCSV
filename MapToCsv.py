37c1bdca58a05913c245e3de8d67322be6119b4b
Ana Soles <ana.soles.valdivia@gmail.com>
2019-08-02 17:35:58 +0200

    Recognize pattern "Site" in .gra file

--- MapToCsv.py
+++ MapToCsv.py
@@ -17,10 +17,23 @@ gra_file = open(sys.argv[1], "r")
 while True:
 	# read velue from input file
 	line = gra_file.readline()
-	# rstrip - remove invisble in the ascci file
+	# 1 rstrip - remove invisble in the ascci file
+	# 2 line is a variable which contain one line on the file NR
 	if len(line) == 0:
 		break
-	print (line.rstrip())
+	# 3 Add .rstrip() to remove the return of each line character (no visible)
+	#4 To print line (string) splitted into array of strings (table)
+#print (line.rstrip().split(" "))
+
+	# 5 Assing variable to the split command - Make a line to an Array ["ANA" "ES" "BONITA"]
+	split_line=(line.rstrip().split(" "))
+# 6 test
+	#print (split_line)
+
+	#7 to find site in the array
+	if split_line[0]=="Site:": 
+		print ("found" )
+
 
 	 #longitude = input_file.readline().rstrip()
 	# if len(longitude) == 0:
